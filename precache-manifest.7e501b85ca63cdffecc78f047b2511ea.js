e905652024f58404ccc08e5cbdc3944d56c23d81
Pierrick Hellequin <pkhv@hotmail.fr>
2022-01-05 21:20:57 +0100

    Updates

---
+++ precache-manifest.7e501b85ca63cdffecc78f047b2511ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62728547a35928d3ef54e81bbf2299c",
+    "url": "/learnblockchain/index.html"
+  },
+  {
+    "revision": "affee4268dd191801f87",
+    "url": "/learnblockchain/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "e7f67a4288ee5a51705b",
+    "url": "/learnblockchain/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "affee4268dd191801f87",
+    "url": "/learnblockchain/static/js/2.4a62d34b.chunk.js"
+  },
+  {
+    "revision": "e7f67a4288ee5a51705b",
+    "url": "/learnblockchain/static/js/main.130d3fb2.chunk.js"
+  },
+  {
+    "revision": "7afabd0f2fdeec2974cc",
+    "url": "/learnblockchain/static/js/runtime-main.07a9845c.js"
+  }
+]);

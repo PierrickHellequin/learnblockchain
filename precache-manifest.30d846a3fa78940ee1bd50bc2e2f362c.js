0c7243387940c460adace6aae21c3992e0f1b280
Pierrick Hellequin <pkhv@hotmail.fr>
2022-01-05 21:47:31 +0100

    Updates

---
+++ precache-manifest.30d846a3fa78940ee1bd50bc2e2f362c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b7d1a13209f8e49bacb7f2762b1f62",
+    "url": "/learnblockchain/index.html"
+  },
+  {
+    "revision": "affee4268dd191801f87",
+    "url": "/learnblockchain/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "87cec57e5d70497fb99c",
+    "url": "/learnblockchain/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "affee4268dd191801f87",
+    "url": "/learnblockchain/static/js/2.4a62d34b.chunk.js"
+  },
+  {
+    "revision": "87cec57e5d70497fb99c",
+    "url": "/learnblockchain/static/js/main.8f387b3d.chunk.js"
+  },
+  {
+    "revision": "7afabd0f2fdeec2974cc",
+    "url": "/learnblockchain/static/js/runtime-main.07a9845c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/learnblockchain/precache-manifest.fd8577930b2b1835135a9184006bcc42.js"
+  "/learnblockchain/precache-manifest.30d846a3fa78940ee1bd50bc2e2f362c.js"
 );
 
 self.addEventListener('message', (event) => {
